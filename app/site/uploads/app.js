f1570f70bdea204a036abd1a33c221557091d962
gmp26 <gmp26@cam.ac.uk>
2012-11-13 16:24:46 +0000

    moved contentPath algorithm to a getpath service

--- app/site/uploads/app.js
+++ app/site/uploads/app.js
@@ -1,22 +1,7 @@
 'use strict';
 /* App Controllers */
 
-// Discover our contentPath 
-var cpath = (function() {
-    var scripts = document.getElementsByTagName('script'),
-        script = scripts[scripts.length - 1],
- 		src;
-
-    if (script.getAttribute.length !== undefined) {
-        src = script.src;
-    }
-	else {
-    	src = script.getAttribute('src', -1);
-	}
-	return "/"+src.split('/').slice(3,-1).join('/');
-}());
-
-var memoryGameApp = angular.module('app', []);
+var memoryGameApp = angular.module('app', ['getpath']);
 
 memoryGameApp.factory('game', function() {
   var tileNames = ['8-ball', 'kronos', 'baked-potato', 'dinosaur', 'rocket', 'skinny-unicorn',
@@ -35,14 +20,14 @@ memoryGameApp.controller('GameCtrl', function GameCtrl($scope, game) {
 //- in the repeater as: <mg-card tile="tile"></mg-card>
 //- card currently being matched as: <mg-card tile="game.firstPick"></mg-card>
 
-memoryGameApp.directive('gameCard', function() {
+memoryGameApp.directive('gameCard', function(contentPath) {
   return {
     restrict: 'A',
 
     template: '<div class="container">' +
 	          '<div class="card" ng-class="{flipped: tile().flipped}">' +
-	              '<img class="front" ng-src="'+cpath+'/img/back.png">' +
-	              '<img class="back" ng-src="'+cpath+'/img/{{tile().title}}.png">' +
+	              '<img class="front" ng-src="'+contentPath+'/img/back.png">' +
+	              '<img class="back" ng-src="'+contentPath+'/img/{{tile().title}}.png">' +
 	          '</div>' +
 	        '</div>',
 
